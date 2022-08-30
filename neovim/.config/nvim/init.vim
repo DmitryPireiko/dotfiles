78f51e1732a84024f0243928a10b1048747be226
Dmitry Sayevich <dima346922336@gmail.com>
2022-08-30 14:25:30 +0300

    full config

---
+++ neovim/.config/nvim/init.vim
@@ -0,0 +1,55 @@
+call plug#begin()
+
+Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
+Plug 'ycm-core/YouCompleteMe'
+"Colorschemes
+Plug 'morhetz/gruvbox'
+Plug 'NLKNguyen/papercolor-theme'
+
+call plug#end()
+
+"Mappings"
+let g:mapleader = ','
+map <C-n> :NERDTreeToggle<CR>
+let NERDTreeShowHidden=1
+
+"Swithing between the windows
+noremap <C-j> <C-w>j
+noremap <C-k> <C-w>k
+noremap <C-l> <C-w>l
+noremap <C-h> <C-w>h
+
+"map <Leader> <Plug>(easymotion-prefix)
+
+"Color Theme
+"colorscheme gruvbox
+set background=dark
+"let g:gruvbox_contrast_dark = 'hard'
+colorscheme PaperColor
+
+"Tabulations
+set expandtab
+set smarttab
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+
+"Search
+set hlsearch
+set incsearch
+
+"Indentations
+"set foldcolumn=2
+
+"Numerations
+set number
+
+"Syntax
+syntax on
+
+"Sound Off
+set noerrorbells
+set novisualbell
+
+"Mouse
+set mouse=a
---
+++ polybar/.config/polybar/openweathermap-fullfeatured.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+get_icon() {
+    case $1 in
+        # Icons for weather-icons
+        01d) icon="";;
+        01n) icon="";;
+        02d) icon="";;
+        02n) icon="";;
+        03*) icon="";;
+        04*) icon="";;
+        09d) icon="";;
+        09n) icon="";;
+        10d) icon="";;
+        10n) icon="";;
+        11d) icon="";;
+        11n) icon="";;
+        13d) icon="";;
+        13n) icon="";;
+        50d) icon="";;
+        50n) icon="";;
+        *) icon="";
+
+        # Icons for Font Awesome 5 Pro
+        #01d) icon="";;
+        #01n) icon="";;
+        #02d) icon="";;
+        #02n) icon="";;
+        #03d) icon="";;
+        #03n) icon="";;
+        #04*) icon="";;
+        #09*) icon="";;
+        #10d) icon="";;
+        #10n) icon="";;
+        #11*) icon="";;
+        #13*) icon="";;
+        #50*) icon="";;
+        #*) icon="";
+    esac
+
+    echo $icon
+}
+
+get_duration() {
+
+    osname=$(uname -s)
+
+    case $osname in
+        *BSD) date -r "$1" -u +%H:%M;;
+        *) date --date="@$1" -u +%H:%M;;
+    esac
+
+}
+
+KEY="e434b5435a979de6e155570590bee89b"
+CITY="Novosibirsk"
+UNITS="metric"
+SYMBOL="°"
+
+API="https://api.openweathermap.org/data/2.5"
+
+if [ -n "$CITY" ]; then
+    if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
+        CITY_PARAM="id=$CITY"
+    else
+        CITY_PARAM="q=$CITY"
+    fi
+
+    current=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
+    forecast=$(curl -sf "$API/forecast?appid=$KEY&$CITY_PARAM&units=$UNITS&cnt=1")
+else
+    location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
+    current=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
+
+fi
+
+if [ -n "$current" ] && [ -n "$forecast" ]; then
+    current_temp=$(echo "$current" | jq ".main.temp" | cut -d "." -f 1)
+    current_icon=$(echo "$current" | jq -r ".weather[0].icon")
+
+
+
+
+    echo " $current_temp$SYMBOL"
+fi
