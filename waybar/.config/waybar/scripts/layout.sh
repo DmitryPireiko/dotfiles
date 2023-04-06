1585ae2ffb47ad7cf0261b23cedd992ad07ba219
sadnessITS <dima346922336@gmail.com>
2023-04-06 22:13:21 +0300

    UPD ranger (disable preview) & waybar (added layout)

---
+++ waybar/.config/waybar/scripts/layout.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+layout=$(hyprctl devices -j | jq '.keyboards[] | select(.name == "ite-tech.-inc.-ite-device(8910)-keyboard")|.active_keymap')
+
+if [ "$layout" == '"Russian"' ]; then
+  echo "ru"
+elif [ "$layout" == '"English (US)"' ]; then
+  echo "en"
+fi
